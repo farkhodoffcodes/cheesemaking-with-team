0298080e5f184f60fea7d6089232c416b2046f90
AzizAmanov0510 <azizbekamanov05@gmail.com>
2024-04-01 17:17:29 +0500

    footer finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Footer from './components/Footer'
+
 
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer />
+      
 
     </BrowserRouter>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../components/Header"
 import Main from "../components/Main"
+import Footer from "../components/Footer"
 const Home = () => {
     return (
         <div>
             <Header/>
             <Main/>
+            <Footer/>
         </div>
     )
 }
